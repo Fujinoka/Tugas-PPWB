e64c014790f076846e92ccb60d687c6d9f862643
Fujinoka <68637268+Fujinoka@users.noreply.github.com>
2020-12-10 21:22:10 +0700

    Update fungsi date.php

--- fungsi date.php
+++ fungsi date.php
@@ -1,19 +1,12 @@
-<?php
-
-function tampil_identitas($nama, $kelas, $alamat)
-{
-    echo 'Nama Saya ' . $nama . '<br>';
-    echo 'Kelas Saya ' . $kelas . '<br>';
-    echo 'Alamat Saya ' . $alamat . '<br>';
-}
-
-function hitung_umur($tahun_lahir, $tahun_sekarang)
-{
-    return $tahun_sekarang - $tahun_lahir;
-}
-
-tampil_identitas('Aldo', 'XI RPL 3', 'Bandung');
-
-$umur_saya = hitung_umur(2004, 2020);
-echo "Umur Saya $umur_saya tahun";
+<?php 
+echo "Ini Format tanggal Indonesia : ";
+echo date("d F Y").'<br>';
+echo "Ini Format tanggal System : ";
+echo date("I, F d, Y").'<br>';
+echo "Ini Format date Database : ";
+echo date("Y-m-d").'<br>';
+echo "Ini Format waktu System : ";
+echo date("g:i A").'<br>';
+echo "Ini Format waktu Indonesia : ";
+echo date("G:i").'<br>';
 ?>
